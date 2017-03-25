279de18c1c47e7b715e10398c1cd505c55e07d2d
Pablo Andrés Dorado Suárez <im@pandres95.me>
2017-03-25 15:24:40 -0500

    Update testing

--- test/modules.js
+++ test/modules.js
@@ -1,15 +1,15 @@
 'use strict';
 
-var expect = require('expect.js');
+var expect = require('chai').expect;
 require('..');
 
 describe('Modules', function(){
 
     it('Loads included modules', function(done){
-        expect(_).to.be.ok();
-        expect(q).to.be.ok();
-        expect(util).to.be.ok();
-        expect(injector).to.be.ok();
+        expect(_).to.be.ok;
+        expect(q).to.be.ok;
+        expect(util).to.be.ok;
+        expect(injector).to.be.ok;
 
         done();
     });
--- test/utils.js
+++ test/utils.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var expect = require('expect.js');
+var expect = require('chai').expect;
 require('..');
 
 describe('Utilities', function(){
 
     it('Loads included utils', function(done){
-        expect(log).to.be.ok();
-        expect(Interface).to.be.ok();
+        expect(log).to.be.ok;
+        expect(Interface).to.be.ok;
 
         done();
     });
